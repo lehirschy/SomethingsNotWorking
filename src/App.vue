c21acb9a36811d9b296ec71f36f74c162f808634
lehirschy <edensjunk@yahoo.com>
2022-02-11 12:11:24 -0600

    Finished App

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,64 @@
-<script setup></script>
+<script setup>
+  import {ref, computed} from "vue";
+
+  const newTodo = ref("")
+  const todos = ref([]);
+
+    const pending = computed (() => {
+      return todos.value.filter((todo) => !todo.done);
+    });
+
+    const completed = computed (() => {
+      return todos.value.filter((todo) => todo.done);
+    });
+
+  const addTodo = () =>{
+    if (newTodo.value.trim())
+    todos.value.push({
+      id: todos.value.length,
+      content: newTodo.value,
+      done: false,
+    });
+    newTodo.value = "";
+  }
+  const changeStatus = (id) => {
+    const todo = todos.value.find((todo) => todo.id === id);
+    todo.done = !todo.done;
+  };
+</script>
 
 <template>
   <div class="min-h-screen bg-gray-800">
-    <div class="container mx-auto flex flex-col space-y-8 pt-8">
-      <h1 class="font-thin text-center text-6xl tracking-tight text-gray-300">My Todo App</h1>
-      <input type="text" class="rounded-lg text-center px-4 py-2 text-xl" placeholder="New Todo"/>
-      <div class="flex justify-between">
+    <div class="container flex flex-col pt-8 mx-auto space-y-8">
+      <h1 class="pb-4 text-6xl font-thin tracking-tight text-center text-gray-300">My Todo App</h1>
+      <input @change="addTodo" 
+        v-model ="newTodo" 
+        type="text" 
+        class="px-4 py-2 text-xl text-center rounded-lg" 
+        placeholder="New Todo"
+        />
+      <div class="flex justify-around">
         <div class="w-1/3">
           <h3 class="text-2xl text-center text-blue-400">Pending</h3>
           <ul class="pt-8 space-y-4">
-            <li class="bg-gray-300 w-full text-center px-4 py-2 rounded-lg ">
-              One
-            </li>
-            <li>
-              Two
+            <li
+            @click="changeStatus(todo.id)" 
+            v-for="todo in pending"
+            :key="todo.id"
+            class="w-full px-4 py-2 font-bold text-center text-blue-600 duration-500 bg-gray-300 rounded-lg hover:cursor-pointer hover:bg-blue-400 hover:text-gray-200">
+              {{ todo.content }}
             </li>
           </ul>
         </div>
         <div class="w-1/3">
           <h3 class="text-2xl text-center text-green-400">Completed</h3>
-          <ul>
-            <li>
-              Three
+          <ul class="pt-8 space-y-4">
+            <li
+            @click="changeStatus(todo.id)"
+            v-for="todo in completed"
+            :key="todo.id"
+            class= "w-full px-4 py-2 font-bold text-center text-green-600 duration-500 bg-gray-300 rounded-lg hover:cursor-pointer hover:bg-green-400 hover:text-gray-200">
+              {{ todo.content }}
             </li>
           </ul>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js}",
-],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },

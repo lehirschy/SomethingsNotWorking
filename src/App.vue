e3a52d095009502cb700ff8445c610701de28ab9
lehirschy <edensjunk@yahoo.com>
2022-02-16 01:23:40 -0600

    Final

--- src/App.vue
+++ src/App.vue
@@ -45,13 +45,13 @@
             @click="changeStatus(todo.id)" 
             v-for="todo in pending"
             :key="todo.id"
-            class="w-full px-4 py-2 font-bold text-center text-blue-600 duration-500 bg-gray-300 rounded-lg hover:cursor-pointer hover:bg-blue-400 hover:text-gray-200">
+            class="w-full px-4 py-2 font-bold text-center text-orange-600 duration-500 bg-gray-300 rounded-lg hover:cursor-pointer hover:bg-orange-400 hover:text-gray-200">
               {{ todo.content }}
             </li>
           </ul>
         </div>
         <div class="w-1/3">
-          <h3 class="text-2xl text-center text-green-400">Completed</h3>
+          <h3 class="text-2xl text-center text-purple-400">Completed</h3>
           <ul class="pt-8 space-y-4">
             <li
             @click="changeStatus(todo.id)"
